0da25caca7d8b224850385e8a578cce97c4373a8
Michal Piotrowski <practicalrs.com@gmail.com>
2024-04-24 23:59:30 +0200

    #2 settings crud p2

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,7 @@
+use std::error::Error;
+
+type Result<T> = std::result::Result<T, Box<dyn Error + Send + Sync>>;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -5,10 +9,12 @@ fn greet(name: &str) -> String {
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
+pub async fn run() -> Result<()> {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
+    Ok(())
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-    prs_ai_cht_lib::run()
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let result = prs_ai_cht_lib::run();
+
+    if let Err(e) = result.await {
+        tracing::error!("Error: {:?}", e);
+    }
+
+    Ok(())
 }
